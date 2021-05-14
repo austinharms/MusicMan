1e62b9df2ec801c994528ff9a3f45d8e9bb0e281
Austin Harms <austin@harmsonline.com>
2021-05-14 15:54:55 -0500

    Add CommandSession

---
+++ CommandSession.js
@@ -0,0 +1,73 @@
+const CommandSession = function(channel = null, users = [], timeout = 60) {
+  this.channel = channel;
+  this.users = users;
+  this.msgFunc = null;
+  this.timeoutFunc = null;
+  this.timeoutDuration = timeout;
+  this.timeoutTimer = setTimeout(this.Timeout.bind(this), this.timeoutDuration * 1000);
+  this.id = ++CommandSession.id;
+};
+
+CommandSession.id = 0;
+CommandSession.sessions = [];
+
+CommandSession.remove = function(id) {
+  CommandSession.sessions = CommandSession.sessions.filter(session => this.session.id !== id);
+};
+
+CommandSession.getSessions = function() {
+  return CommandSession.sessions;
+};
+
+CommandSession.create = function(...prams) {
+  const s = new CommandSession(...prams);
+  this.sessions.add(s);
+  return s;
+};
+
+CommandSession.sendSessionMsg = function(msg) {
+  const foundSession = this.sessions.find(s => s.checkForSession(msg));
+  if (foundSession) {
+    foundSession.sendMsg(msg);
+    return true;
+  }
+
+  return false;
+};
+
+CommandSession.prototype.checkForSession = function(msg) {
+  if (this.channel != null)
+    return msg.channel.id === this.channel.id && (this.users.length === 0 || this.users.find(user => user.id.equals(msg.author.id)));
+  else
+    return !!this.users.find(userID.equals(msg.author.id));
+};
+
+CommandSession.prototype.onMsg = function(f) {
+  this.msgFunc = f;
+};
+
+CommandSession.prototype.onTimeout = function(f) {
+  this.timeoutFunc = f;
+  this.stop();
+};
+
+CommandSession.prototype.sendMsg = function(msg) {
+  this.resetTimeout();
+  if (this.msgFunc !== null) this.msgFunc(msg, this);
+};
+
+CommandSession.prototype.stop = function() {
+  clearTimeout(this.timeoutTimer);
+  CommandSession.remove(this.id);
+};
+
+CommandSession.Timeout = function() {
+  if (this.timeoutFunc !== null) this.timeoutFunc(this);
+};
+
+CommandSession.prototype.resetTimeout = function() {
+  clearTimeout(this.timeoutTimer);
+  this.timeoutTimer = setTimeout(this.Timeout.bind(this), this.timeoutDuration * 1000);
+};
+
+export default CommandSession;
--- app.js
+++ app.js
@@ -2,19 +2,36 @@ require('dotenv').config();
 const token = process.env.BOT_TOKEN;
 const Discord = require('discord.js');
 const client = new Discord.Client();
+import CommandSession from "./CommandSession"; 
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content.startsWith("~hi")) {
-    msg.channel.send("Hi!");
-  } else if (!msg.content.toLocaleLowerCase().includes('lie') && msg.author.id === '385283295933628419') {
-    msg.reply('He LIES!');
-  } else if (msg.author.id === "565960196313317376" && msg.deletable) {
-    //msg.delete();
+const COMMANDS = Object.freeze({
+  "hi": (props, user, channel, msg) => {
+    channel.send("Hi!");
+  },
+  "say": (props, user, channel, msg) => {
+    channel.send(props.join(" "));
+  },
+  "start": (props, user, channel, msg) => {
+    CommandSession.create(channel, [], 10);
   }
 });
 
+
+const parseCommand = msg => {
+  if (!msg.content.startsWith("~")) return;
+  msg.content = msg.content.substring(1);
+  if (CommandSession.sendSessionMsg(msg)) return;
+  const props = msg.content.split(" ");
+  const command = props.shift();
+  if (COMMANDS[command] !== undefined) {
+    try {
+      COMMANDS[command](props, msg.author, msg.channel, msg);
+    } catch(e) {
+      console.log("Error running command: " + command + " Error: " + e + " MSG: " + msg);
+    }
+  }
+};
+
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
+client.on('message', parseCommand);
 client.login(token);
