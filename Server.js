44be7290c35ea86d7f623cd2f0e459c90a070445
Austin Harms <austin@harmsonline.com>
2021-09-20 11:00:06 -0500

    Add BotError to Server Class

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const getCommand = require("./CommandList");
 const Audio = require("./Audio");
+const BotError = require("./Error");
 
 const Server = function (guildId) {
   this.channel = null;
@@ -16,10 +17,12 @@ Server.prototype.getCommandAudioInstance = function(msg) {
     if (client !== undefined) return client;
     client = this.audioClients.find(a => !a.getConnected());
     if (client !== undefined) return client;
-    this.sendError("Out of Bots to Join", "Out of Clients");
+    const bError = BotError.createError("Out of Bots to Join", new Error("Out of Audio Clients to Use"), msg.author.id, this.id, "Server:getCommandAudioInstance", true);
+    this.sendError(bError);
     return false;
   } catch(e) {
-    this.sendError("Failed to Run Audio Command", e);
+    const bError = BotError.createError("Failed to Run Audio Command", e, msg.author.id, this.id, "Server:getCommandAudioInstance", false);
+    this.sendError(bError);
     return false;
   }
 };
@@ -28,13 +31,15 @@ Server.prototype.getCommandUserInVC = function(msg) {
   try {
     let client = this.audioClients.find(a => a.checkUserInChat(msg.author.id));
     if (client === undefined) {
-      this.sendError("You Must be in a Bot VC to Run this Command", "User Error");
+      const bError = BotError.createError("You Must be in a Bot VC to Run this Command", new Error("User Attempted to run Command without an Audio Instance Connected"), msg.author.id, this.id, "Server:getCommandUserInVC", true);
+      this.sendError(bError);
       return false;
     }
 
     return true;
   } catch(e) {
-    this.sendError("Failed to Run Audio Command", e);
+    const bError = BotError.createError("Failed to Run Audio Command", e, msg.author.id, this.id, "Server:getCommandUserInVC", false);
+    this.sendError(bError);
     return false;
   }
 };
@@ -53,13 +58,14 @@ Server.prototype.receivedMessage = async function(msg, prefix) {
         const params = msgParts.join(" ");
         await command.run(this, params, msg);
       } catch(e) {
-        this.sendError("Failed to Run Command: " + command.getName(),e);
+        const bError = BotError.createError("Failed to Run Command: " + command.getName(), e, msg.author.id, this.id, "Server:receivedMessage", false);
+        this.sendError(bError);
       }
     } else {
       this.sendEmbed("What?", "Unknown Command\nUse the Help Command to View All Commands");
     }
   } catch(e) {
-    console.log("Bot Receive Error: " + e);
+    BotError.createError("Bot Receive Error", e, msg.author.id, this.id, "Server:receivedMessage", false);
   }
 
   this.msg = null;
@@ -69,7 +75,8 @@ Server.prototype.sendRaw = function(msg) {
   try {
     this.channel.send(msg);
   } catch (e) {
-    this.sendError("Failed to send Message", e);
+    const bError = BotError.createError("Failed to send Message", e, -1, this.id, "Server:sendRaw", false);
+    this.sendError(bError);
   }
 };
 
@@ -89,7 +96,8 @@ Server.prototype.sendEmbed = function (title, description, imageURL = null) {
       embed.embed.image = { url: imageURL };
     this.channel.send(embed);
   } catch (e) {
-    this.sendError("Failed to send Embed", e);
+    const bError = BotError.createError("Failed to send Embed", e, -1, this.id, "Server:sendEmbed", false);
+    this.sendError(bError);
   }
 };
 
@@ -97,22 +105,22 @@ Server.prototype.thumbsUp = function() {
   try {
     this.msg.react("👍");
   } catch (e) {
-    this.sendError("Failed to add Thumbs Up Reaction", e);
+    const bError = BotError.createError("Failed to add Thumbs Up Reaction", e, this.msg.author.id, this.id, "Server:thumbsUp", false);
+    this.sendError(bError);
   }
 };
 
-Server.prototype.sendError = function(errMsg, err) {
+Server.prototype.sendError = function(botError) {
   try {
     this.channel.send({
       embed: {
         title: "There was an Error!",
         color: "RED",
-        description: errMsg,
+        description: botError.msg,
       },
     });
-    throw new Error(err);
   } catch (e) {
-    console.log(`Bot Error: ${errMsg}, True Error: ${err}, Send Error: ${e}`);
+    BotError.createError("Failed to Send Error embed", e, -1, this.id, "Server:sendError", false);
   }
 };
 
