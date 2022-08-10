fd6864826e0220a1631473ff5eceddd2974b2ea6
Austin Harms <austin@harmsonline.com>
2022-08-09 21:32:19 -0500

    Add command error catching

--- bot.ts
+++ bot.ts
@@ -1,30 +1,56 @@
 import { config } from "./configuration";
-import { getCommand, loadCommands, publishSlashCommands } from "./commandManager";
-import { ChatInputCommandInteraction, Client, GatewayIntentBits, Interaction, MessageContextMenuCommandInteraction } from "discord.js";
+import {
+  getCommand,
+  loadCommands,
+  publishSlashCommands,
+} from "./commandManager";
+import {
+  ChatInputCommandInteraction,
+  Client,
+  GatewayIntentBits,
+  Interaction,
+} from "discord.js";
 import { Command } from "./command";
+import { createErrorEmbed } from "./messageUtilities";
 
 const createClient = async (token: string): Promise<Client> => {
-    const client: Client = new Client({ intents: [GatewayIntentBits.Guilds] });
-    const ready: Promise<any> = new Promise((a) => client.on("ready", a));
-    await client.login(token);
-    console.log(`Bot "${client.user?.tag}" Ready`);
-    await ready;
-    return client;
+  const client: Client = new Client({ intents: [GatewayIntentBits.Guilds] });
+  const ready: Promise<any> = new Promise((a) => client.on("ready", a));
+  await client.login(token);
+  console.log(`Bot "${client.user?.tag}" Ready`);
+  await ready;
+  return client;
 };
 
 (async (): Promise<void> => {
-    await loadCommands();
-    //await publishSlashCommands();
-    const client: Client = await createClient(config.discord.bots[0].token);
-    client.on("interactionCreate",async (interaction: Interaction) => {
-        if (interaction.isChatInputCommand()) {
-            const chatInteraction: ChatInputCommandInteraction = interaction as ChatInputCommandInteraction;
-            const command: Command | null = getCommand(chatInteraction.commandName);
-            if (command !== null) {
-                 await command.run(chatInteraction);
-            } else {
-                await chatInteraction.reply('Invalid Command');
-            }
+  await loadCommands();
+  await publishSlashCommands();
+  const client: Client = await createClient(config.discord.bots[0].token);
+  client.on("interactionCreate", async (interaction: Interaction) => {
+    try {
+      if (interaction.isChatInputCommand()) {
+        const chatInteraction: ChatInputCommandInteraction =
+          interaction as ChatInputCommandInteraction;
+        const command: Command | null = getCommand(chatInteraction.commandName);
+        if (command !== null) {
+          try {
+            await command.run(chatInteraction);
+          } catch (e: any) {
+            if (interaction.channel?.isTextBased())
+              await interaction.channel.send({
+                embeds: [
+                  createErrorEmbed(`Failed to run command "${command.name}"`),
+                ],
+              });
+          }
+        } else {
+          await chatInteraction.reply("Invalid Command");
         }
-    });
+      }
+    } catch (e: any) {
+      console.error(
+        `Error processing interaction:\n${interaction}\nError:\n${e}`
+      );
+    }
+  });
 })();
