dfd9780a987137adc8e775f22109fd86d19d8633
Austin Harms <austin@harmsonline.com>
2022-08-13 22:05:18 -0500

    Add queue command

---
+++ commands/queue.ts
@@ -0,0 +1,128 @@
+import {
+  ChatInputCommandInteraction,
+  GuildMember,
+  VoiceChannel,
+  ChannelType,
+  GatewayIntentBits,
+  InteractionResponse,
+} from "discord.js";
+import { BotError } from "../BotError";
+import { Command } from "../command";
+import { createEmbed, createBotErrorEmbed } from "../messageUtilities";
+import { Song } from "../song";
+import {
+  getVoiceConnectionInterface,
+  VoiceConnectionInterface,
+} from "../voiceManager";
+
+const queue: Command = {
+  name: "queue",
+  description: "View the current song queue",
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+  options: [
+    {
+      type: 4,
+      name: "page",
+      required: false,
+      minValue: 1,
+      description: "What page to view, default: 1",
+    },
+  ],
+  run: async (interaction: ChatInputCommandInteraction) => {
+    // Don't need to wait for this to process the rest of the command
+    const loadingReply: Promise<InteractionResponse<boolean>> =
+      interaction.reply({
+        ephemeral: true,
+        embeds: [createEmbed("Queue", "Loading...")],
+      });
+
+    try {
+      const member: GuildMember = interaction.member as GuildMember;
+      if (
+        !member.voice.channel ||
+        member.voice.channel.type !== ChannelType.GuildVoice
+      )
+        throw new BotError(
+          "Queue command user not in bot voice channel",
+          "You must be in a bot voice channel"
+        );
+
+      const voiceChannel: VoiceChannel = member.voice.channel as VoiceChannel;
+      const connectionInterface: VoiceConnectionInterface | null =
+        getVoiceConnectionInterface(voiceChannel);
+      if (!connectionInterface)
+        throw new BotError(
+          "Queue command user not in bot voice channel",
+          "You must be in a bot voice channel"
+        );
+      const page: number = interaction.options.getInteger("page", false) || 1;
+      const current: Song | undefined =
+        connectionInterface.getCurrentSongStream()?.song;
+      if (!current) {
+        await loadingReply;
+        await interaction.editReply({
+          embeds: [createEmbed("Queue", "Nothings here")],
+        });
+        return;
+      }
+
+      const SONGS_PER_PAGE: number = 20;
+      const songs: Song[] = connectionInterface.queue;
+      const totalPageCount = Math.ceil(songs.length / SONGS_PER_PAGE);
+      if (page < 1 || page > totalPageCount) {
+        throw new BotError(
+          "Queue command page out of range",
+          `Page out of range: min: 1, max: ${totalPageCount}`
+        );
+      }
+
+      const pageSongs: Song[] = songs.slice(
+        (page - 1) * SONGS_PER_PAGE,
+        (page - 1) * SONGS_PER_PAGE + SONGS_PER_PAGE
+      );
+      // need to wait for reply before we can edit it
+      await loadingReply;
+      if (page !== 1) {
+        await interaction.editReply({
+          embeds: [
+            createEmbed(
+              "Queue",
+              `${pageSongs.reduce(
+                (acc: string, { title, url }: Song, i: number) =>
+                  `${acc}   *${
+                    i + 1 + (page - 1) * SONGS_PER_PAGE
+                  }*: [${title}](${url})\n`,
+                ""
+              )}\n *Page ${page} of ${totalPageCount}*`
+            ),
+          ],
+        });
+      } else {
+        await interaction.editReply({
+          embeds: [
+            createEmbed(
+              "Queue",
+              `***Now Playing***: [${current.title}](${
+                current.url
+              })\n***Queue***:\n ${pageSongs.reduce(
+                (acc: string, { title, url }: Song, i: number) =>
+                  `${acc}   *${i + 1}*: [${title}](${url})\n`,
+                ""
+              )}\n *Page 1 of ${totalPageCount}*`,
+              current.thumbnail?.href
+            ),
+          ],
+        });
+      }
+    } catch (e: any) {
+      if (!(e instanceof BotError)) e = new BotError(e, "Failed to get queue");
+      // need to wait for reply before we can edit it
+      await loadingReply;
+      await interaction.editReply({
+        embeds: [createBotErrorEmbed(e)],
+      });
+    }
+  },
+};
+
+export default queue;
