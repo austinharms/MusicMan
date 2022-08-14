7bb1c9cb9dc163c74deb576c1fdc316dffdff197
Austin Harms <austin@harmsonline.com>
2022-08-13 22:05:26 -0500

    Add skip command

---
+++ commands/skip.ts
@@ -0,0 +1,78 @@
+import {
+  ChatInputCommandInteraction,
+  GuildMember,
+  VoiceChannel,
+  ChannelType,
+  GatewayIntentBits,
+  InteractionResponse,
+} from "discord.js";
+import { BotError } from "../BotError";
+import { Command } from "../command";
+import { createEmbed, createBotErrorEmbed } from "../messageUtilities";
+import {
+  getVoiceConnectionInterface,
+  VoiceConnectionInterface,
+} from "../voiceManager";
+
+const skip: Command = {
+  name: "skip",
+  description: "skips songs",
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+  options: [
+    {
+      type: 4,
+      name: "count",
+      required: false,
+      minValue: 1,
+      description: "How many songs to skip, default: 1",
+    },
+  ],
+  run: async (interaction: ChatInputCommandInteraction) => {
+    // Don't need to wait for this to process the rest of the command
+    const loadingReply: Promise<InteractionResponse<boolean>> =
+      interaction.reply({
+        ephemeral: true,
+        embeds: [createEmbed("Skip", "Loading...")],
+      });
+
+    try {
+      const member: GuildMember = interaction.member as GuildMember;
+      if (
+        !member.voice.channel ||
+        member.voice.channel.type !== ChannelType.GuildVoice
+      )
+        throw new BotError(
+          "Skip command user not in bot voice channel",
+          "You must be in a bot voice channel"
+        );
+
+      const voiceChannel: VoiceChannel = member.voice.channel as VoiceChannel;
+      const connectionInterface: VoiceConnectionInterface | null =
+        getVoiceConnectionInterface(voiceChannel);
+      if (!connectionInterface)
+        throw new BotError(
+          "Skip command user not in bot voice channel",
+          "You must be in a bot voice channel"
+        );
+      const count: number = interaction.options.getInteger("count", false) || 1;
+      await connectionInterface.skipSongs(count);
+      // need to wait for reply before we can edit it
+      await loadingReply;
+      await interaction.editReply({
+        embeds: [
+          createEmbed("Skip", `${count} Song${count > 1 ? "s" : ""} skipped`),
+        ],
+      });
+    } catch (e: any) {
+      if (!(e instanceof BotError)) e = new BotError(e, "Failed to skip songs");
+
+      // need to wait for reply before we can edit it
+      await loadingReply;
+      await interaction.editReply({
+        embeds: [createBotErrorEmbed(e)],
+      });
+    }
+  },
+};
+
+export default skip;
